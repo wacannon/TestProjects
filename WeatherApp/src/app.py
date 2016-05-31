7935c44b5dda5195fbc09e57f23736018f54f2b6
Aaron <blue.steele@gmail.com>
2016-05-31 13:35:57 -0400

    added weather api

--- WeatherApp/src/app.py
+++ WeatherApp/src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session
-
+from openweatherapi import getdailyforecast
 import sys, dateutil
 
 
@@ -10,7 +10,12 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.secret_key = 'why would I tell you my secret key?'
 
+
 @app.route("/")
 def main():
-    data = dayforcast()
+    data = getdailyforecast()
     return render_template('index.html', data=data)
+
+
+if __name__ == "__main__":
+    app.run()
