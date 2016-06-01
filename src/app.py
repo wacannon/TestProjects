3a9ffb708adbeb1dbd448eba5df9e01e04e64f23
Aaron <blue.steele@gmail.com>
2016-06-01 14:00:18 -0400

    changed procfile

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from openweatherapi import getdailyforecast
-import sys, datetime
+import sys, datetime, os
 
 
 reload(sys)
@@ -23,4 +23,5 @@ def _jinja2_filter_datetime(date):
     return shortdate
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
